4c3e91fe5c8b05d16ba146e829d610acb9207d73
Asmita Mallik <asmita438@gmail.com>
2020-08-27 00:30:21 +0545

    test to search for a variable from search bar

---
+++ tests/AMZN_Search_TC_002.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# declare variable to store the URL to be visited
+base_url="https://www.amazon.in"
+# declare variable to store search term
+search_term="Foundation of Software Testing by Dorothy Graham and Rex Black"
+
