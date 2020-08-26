77f32f9fb2957bd1bbcbc738266596f9729a1d1d
Asmita Mallik <asmita438@gmail.com>
2020-08-27 00:32:26 +0545

    steps to initialize web driver and open base_url

--- tests/AMZN_Search_TC_002.py
+++ tests/AMZN_Search_TC_002.py
@@ -4,4 +4,9 @@ from selenium.webdriver.common.keys import Keys
 base_url="https://www.amazon.in"
 # declare variable to store search term
 search_term="Foundation of Software Testing by Dorothy Graham and Rex Black"
-
+driver=webdriver.Chrome(executable_path="/home/asmita/Desktop/Amazon/drivers/geckodriver")
+driver.maximize_window()
+driver.implicitly_wait(10) 
+driver.get(base_url)
+# test whether correct URL/ Web Site has been loaded or not
+assert "Amazon" in driver.title
