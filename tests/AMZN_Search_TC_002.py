0425e499e8ee1a76d599af8883250a8d1c17d4f7
Asmita Mallik <asmita438@gmail.com>
2020-08-27 00:34:52 +0545

    added steps to search for search_term

--- tests/AMZN_Search_TC_002.py
+++ tests/AMZN_Search_TC_002.py
@@ -10,3 +10,18 @@ driver.implicitly_wait(10)
 driver.get(base_url)
 # test whether correct URL/ Web Site has been loaded or not
 assert "Amazon" in driver.title
+# to enter search term, we need to locate the search textbox
+searchTextBox=driver.find_element_by_id("twotabsearchtextbox")
+# to clear any text in the search textbox
+searchTextBox.clear()
+# to enter the search term in the search textbox via send_keys() function
+searchTextBox.send_keys(search_term)
+# to search for the entered search term
+searchTextBox.send_keys(Keys.RETURN)
+# to verify if the search results page loaded
+assert f"Amazon.in:{search_term}" in driver.title
+# to verify if the search results page contains any results or no results were found.
+assert "No results found." not in driver.page_source
+# -- Post - Condition --
+# to close the browser
+driver.close()
